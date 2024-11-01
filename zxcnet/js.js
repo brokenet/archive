0a6f85f6239ecdb191f38813990f5f81fda8d44f
brokenet <wearebrokenet@gmail.com>
2024-11-01 12:27:21 +0300

    Add files via upload

---
+++ zxcnet/js.js
@@ -0,0 +1,43 @@
+function sleep(){
+  window.addEventListener('load', () => {
+    document.querySelector(".anim").classList.add("anim3")
+  });
+}
+setInterval(sleep, 1000);
+setTimeout(function() {
+  document.querySelector(".anim").classList.add("anim3")
+}, 200);
+function sleep(){
+  window.addEventListener('load', () => {
+    document.querySelector(".anim").classList.add("anim3")
+  });
+}
+setInterval(sleep, 1000);
+setTimeout(function() {
+  document.querySelector(".anim").classList.add("anim3")
+}, 500);
+function sleep(){
+  window.addEventListener('load', () => {
+    document.querySelector(".anim").classList.add("anim4")
+  });
+}
+setInterval(sleep, 1000);
+setTimeout(function() {
+  document.querySelector(".anim").classList.add("anim4")
+}, 1000);
+function sleep(){
+    window.addEventListener('load', () => {
+        document.querySelector(".anim").classList.add("anim2")
+      });
+}
+  setInterval(sleep, 1000);
+  setTimeout(function() {
+    document.querySelector(".anim").classList.add("anim2")
+}, 2000);
+
+
+let a = document.querySelector(".rr").innerHTML
+
+function dd(){
+  window.open("jkejf82.html");
+}
